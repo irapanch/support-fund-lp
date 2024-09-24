1edb03a7ba2b5a48a27b5b71c7d27bc91c282100
irapanch <117355580+irapanch@users.noreply.github.com>
2024-09-24 19:32:37 +0300

    subscribe template

---
+++ js/subscribe.js
@@ -0,0 +1,24 @@
+// document
+//   .getElementById("subscribeForm")
+//   .addEventListener("submit", function (event) {
+//     event.preventDefault(); // Останавливаем отправку формы
+
+//     // Открываем модальное окно
+//     document.getElementById("modal").style.display = "flex";
+//     document.getElementById("backdrop").style.display = "block";
+//   });
+
+// document.getElementById("closeModal").addEventListener("click", closeModal);
+// document.getElementById("backdrop").addEventListener("click", closeModal);
+// document.addEventListener("keydown", function (event) {
+//   if (event.key === "Escape") {
+//     closeModal();
+//   }
+// });
+
+// function closeModal() {
+//   document.getElementById("modal").style.display = "none";
+//   document.getElementById("backdrop").style.display = "none";
+//   // Прокручиваем страницу вверх к хедеру
+//   window.scrollTo({ top: 0, behavior: "smooth" });
+// }
