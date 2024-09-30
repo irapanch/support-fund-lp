8ec7a68795c5880b416906e9efc182b433a086c7
irapanch <117355580+irapanch@users.noreply.github.com>
2024-09-30 09:39:01 +0300

    subscribe

--- js/subscribe.js
+++ js/subscribe.js
@@ -1,24 +1,28 @@
-// document
-//   .getElementById("subscribeForm")
-//   .addEventListener("submit", function (event) {
-//     event.preventDefault(); // Останавливаем отправку формы
+document
+  .getElementById("subscribeForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault(); // Останавливаем отправку формы
 
-//     // Открываем модальное окно
-//     document.getElementById("modal").style.display = "flex";
-//     document.getElementById("backdrop").style.display = "block";
-//   });
+    // Открываем модальное окно
+    document.getElementById("modalSubscribe").style.display = "flex";
+    document.getElementById("backdrop").style.display = "flex";
+    // Очищаем форму
+    event.target.reset();
+  });
 
-// document.getElementById("closeModal").addEventListener("click", closeModal);
-// document.getElementById("backdrop").addEventListener("click", closeModal);
-// document.addEventListener("keydown", function (event) {
-//   if (event.key === "Escape") {
-//     closeModal();
-//   }
-// });
+document
+  .getElementById("modalSubscribeClose")
+  .addEventListener("click", closeModalSubs);
+document.getElementById("backdrop").addEventListener("click", closeModalSubs);
+document.addEventListener("keydown", function (event) {
+  if (event.key === "Escape") {
+    closeModalSubs();
+  }
+});
 
-// function closeModal() {
-//   document.getElementById("modal").style.display = "none";
-//   document.getElementById("backdrop").style.display = "none";
-//   // Прокручиваем страницу вверх к хедеру
-//   window.scrollTo({ top: 0, behavior: "smooth" });
-// }
+function closeModalSubs() {
+  document.getElementById("modalSubscribe").style.display = "none";
+  document.getElementById("backdrop").style.display = "none";
+  // Прокручиваем страницу вверх к хедеру
+  window.scrollTo({ top: 0, behavior: "smooth" });
+}
